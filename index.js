3bcfa9ebaad6d419d1d30631b0098677cd70efab
Maicol-Perdomo <maicolperdomocesar@gmail.com>
2024-12-15 17:41:02 -0300

    .bat
    se empezo el .ba para iniciar proyecto
    y comentarios en index

---
+++ index.js
@@ -0,0 +1,12 @@
+// Conexion a bbdd
+
+
+// Crear Servidor node
+
+// Configurar cors
+
+// COnvertir los datos del body a objetos js
+
+// Cargar conf rutas
+
+// Poner servidor a escuchar peticiones http
