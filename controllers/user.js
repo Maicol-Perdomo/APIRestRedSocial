c510e71d5ed1e6dcbd3030cf0093ddf3812c3a9f
maicol-perdomo <maicolperdomocesar@gmail.com>
2024-12-18 16:37:23 -0300

    54. Accion y ruta de registro de usuarios

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,5 @@
+// Importar dependencias y modulos
+const User = require("../models/user")
 
 // Acciones de prueba
 const pruebaUser = (req, res) =>{
@@ -7,21 +9,56 @@ const pruebaUser = (req, res) =>{
 }
 
 // Registro de usuarios
-const register = (req, res) =>{
+const register = async (req, res) =>{
     // recoger datos de la peticion
-
+    let params = req.body;
     // Comprobar que me llegan bien (+ validacion)
+    if(!params.name || !params.email || !params.password || !params.nick){
+        return res.status(400).json({
+            status: "error",
+            message: "Faltan datos por enviar"
+        })
+    };
 
-    // Control usuarios duplicados
+    // Crear objeto de usuario
+    let user_to_save = new User(params);
+    try{
+        // Control de usuarios duplicados 
+        /*
+          Todas las condiciones del or se tienen que cumplir una u otra
+          Si existe una u otra, es que existe el usuario
+        */
+        const users = await User.find({ 
+            $or: 
+                [
+                    {email: user_to_save.email.toLowerCase()},
+                    {nick: user_to_save.nick.toLowerCase()}
+            ]
+        });
 
-    // Cifrar la contraseña
+		if(users && users.length >= 1){
+			return res.status(200).send({
+				status: "success",
+				message:"El usuario ya existe"
+			});
+		}      
+        // Cifrar la contraseña
 
-    // Guardar usuario en la bbdd
+        // Guardar usuario en la bbdd
 
-    // Devolver resultado
-    return res.status(200).json({
-        message: "Accion de registro de usuarios"
-    });
+        // Devolver resultado
+        return res.status(200).json({
+            status: "success",
+            message: "Accion de registro de usuarios",
+            params,
+            user_to_save
+        });
+    }catch(error){
+        return res.status(500).json({
+            status: "error",
+            message: "Error en la consulta"
+        });
+    }
 }
 
 // Exportar acciones
