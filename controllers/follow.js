d9ad28c2c2c089e4ccfb774fb8178479b2d67db5
maicol-perdomo <maicolperdomocesar@gmail.com>
2024-12-30 18:29:31 -0300

    73. Modelo y controllador de follows

--- controllers/follow.js
+++ controllers/follow.js
@@ -1,9 +1,20 @@
+const Follow = require("../models/follow")
+const User = require("../models/user")
+
 // Acciones de prueba
 const pruebaFollow = (req, res) =>{
     return res.status(200).send({
         message: "Mensaje enviado desde: controllers/follow.js"
     })
 }
+
+// Accion de guardar un follow (accion seguir)
+
+// Accion de borrar un follow (accion dejar de seguir)
+
+// Accion listado de usuarios que estoy siguiendo
+
+// Accion listado de usuarios que me siguen
 
 // Exportar acciones
 module.exports={
