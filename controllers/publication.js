c743351b4b7516ac45e17064605e64b8c90510e2
maicol-perdomo <maicolperdomocesar@gmail.com>
2025-01-06 11:36:02 -0300

    84. Modelo de publicaciones

--- controllers/publication.js
+++ controllers/publication.js
@@ -1,9 +1,27 @@
+// Importar modelo
+const Publication = require("../models/publication");
+
 // Acciones de prueba
 const pruebaPublication = (req, res) =>{
     return res.status(200).send({
         message: "Mensaje enviado desde: controllers/publication.js"
     })
 }
+
+// Guardar publicacion
+
+// Sacar una publicacion
+
+// Eliminar publicaciones
+
+// Listar todas las publicaciones
+
+// Listar publicaciones de un usuario
+
+// Subir ficheros
+
+// Devolver archivos multimedia imagenes
+
 
 // Exportar acciones
 module.exports={
