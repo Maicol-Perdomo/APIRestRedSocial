4bd6fe4ba4a086b855a92a1eea025afac727d9c2
maicol-perdomo <maicolperdomocesar@gmail.com>
2025-01-11 23:35:19 -0300

    85. Guardar Publicaciones

--- controllers/publication.js
+++ controllers/publication.js
@@ -9,6 +9,43 @@ const pruebaPublication = (req, res) =>{
 }
 
 // Guardar publicacion
+const save = async (req, res) =>{
+    // Recoger datos del body
+    const params = req.body
+    // Si no llegan dar respuesta negativa
+    if(!params.text) return res.status(400).send({
+        status: "error",
+        message: "Debes enviar el texto de la publicacion"
+    });
+    
+    // Crear y rellenar el objeto del modelo 
+    let newPublication = new Publication(params);
+    newPublication.user = req.user.id
+    
+    // Guardar objeto en bbdd
+    try{
+        let publicationStored = await newPublication.save();
+        if(!publicationStored){
+            return res.status(500).send({
+                status: "error",
+                message: "No se ha creado la publicacion"
+            })
+        }
+
+        // Devolver respuesta
+        return res.status(200).send({
+            status: "success",
+            message: "publicacion guardada",
+            publication: publicationStored
+        });
+    }catch(e){
+        return res.status(500).send({
+            status: "error",
+            message: "Ah ocurrido un error en la consulta",
+            e
+        })
+    }
+}
 
 // Sacar una publicacion
 
@@ -25,5 +62,6 @@ const pruebaPublication = (req, res) =>{
 
 // Exportar acciones
 module.exports={
-    pruebaPublication
+    pruebaPublication,
+    save
 }
--- routes/publication.js
+++ routes/publication.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const PublicationController = require("../controllers/publication");
+const check = require("../middelwares/auth");
 
 // Definir Rutas
 router.get("/prueba-publication", PublicationController.pruebaPublication);
+router.post("/save", check.auth, PublicationController.save);
 
 // Exportar Rutas
 module.exports = router;
